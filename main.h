b70ea1cf7eb50156c7958375dbc8aa4ef7ae13f6
fellohodhis <odhiambof446@gmail.com>
2022-10-19 02:34:03 -0700

    Updated header file

--- main.h
+++ main.h
@@ -2,31 +2,38 @@
 #define _MAIN_H_
 
 #include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
 /**
- * struct print - structure for printing various types
- * @t: type to print
- * @f: function to print
+ * struct print - struct for print function
+ *
+ * @conv_spec: conversion specifier.
+ * @f: pointer to the printer function
+ *
+ * Return: none
  */
 typedef struct print
 {
-	char *t;
-	int (*f)(va_list);
+	char *conv_spec;
+	int (*f)(va_list, char *);
 } print_t;
 
-int _putchar(char c);
+int (*get_pnt_funct(const char *s, int *pos))(va_list, char *);
+int print_char(va_list args, char *buffer);
+int print_str(va_list args, char *buffer);
+int print_int(va_list args, char *buffer);
+int print_unint(va_list args, char *buffer);
 int _printf(const char *format, ...);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_i(va_list i);
-int print_d(va_list d);
-int print_u(va_list u);
-int print_b(va_list b);
-int print_o(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_p(va_list p);
-int print_S(va_list S);
-int print_r(va_list r);
-int print_R(va_list R);
+int _print_buf(char *buf, int n);
+int print_bin(va_list args, char *buffer);
+int print_address(va_list, char *buffer);
+int print_shex(va_list, char *buffer);
+int print_chex(va_list, char *buffer);
+int print_octal(va_list, char *buffer);
+int custom_str(va_list, char *buffer);
+int pnt_plus_int(va_list, char *buffer);
+int pnt_space_int(va_list, char *buffer);
+int pnt_o_octal(va_list, char *buffer);
+int pnt_upp_hex(va_list, char *buffer);
 
 #endif
