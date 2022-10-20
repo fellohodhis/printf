479fa6ce3fbc07f668e9b6b124dd428396609da5
AchiraAlx <111626469+AchiraALX@users.noreply.github.com>
2022-10-20 14:28:11 +0300

    Create check_null.c

--- main.h
+++ main.h
@@ -7,7 +7,11 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
+void check_null(const char *format);
 int print_rot(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
+int print_reverse(va_list types, char buffer[],
+                  int flags, int width, int precision, int size);
+                
 
 #endif /* MAIN_H */
